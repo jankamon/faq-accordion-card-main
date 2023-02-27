a60e92ad8a460cc027cd1db78a2ae60be04c40de
janekk <janekkamon@gmail.com>
2023-02-27 12:29:09 +0100

    added bolding headers, known bug: overflowing after opening all faq

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 const hiddenText = document.getElementsByClassName('hidden-text');
+const textBlock = document.getElementsByClassName('text-block');
+const headers = document.querySelectorAll('h2');
 const btn = document.getElementsByClassName('btn');
 
-console.log(hiddenText);
-console.log(btn);
+console.log(headers);
 
 const toggleText = (i) => {
     if(hiddenText[i].classList.contains('hidden')){
         hiddenText[i].classList.remove('hidden');
+        headers[i].style.fontWeight = '700';
     } else {
         hiddenText[i].classList.add('hidden');
+        headers[i].style.fontWeight = '400';
     }
 }
 
-for (let i = 0; i < btn.length; i++){
-    btn[i].addEventListener('click', function(event) {
+for (let i = 0; i < textBlock.length; i++){
+    textBlock[i].addEventListener('click', function() {
         toggleText(i);
-        console.log(event.target + i);
     });
 }

bfdff90b73692ae449ce90f32b600baa1ef6904d
janekk <janekkamon@gmail.com>
2023-02-27 13:23:51 +0100

    closing other faq

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const hiddenText = document.getElementsByClassName('hidden-text');
 const textBlock = document.getElementsByClassName('text-block');
 const headers = document.querySelectorAll('h2');
-const btn = document.getElementsByClassName('btn');
 
-console.log(headers);
+const hideAll = () => {
+    for(let i = 0; i < textBlock.length; i++){
+        hiddenText[i].classList.add('hidden');
+        headers[i].style.fontWeight = '400';
+    }
+}
 
 const toggleText = (i) => {
     if(hiddenText[i].classList.contains('hidden')){
@@ -17,6 +21,7 @@ const toggleText = (i) => {
 
 for (let i = 0; i < textBlock.length; i++){
     textBlock[i].addEventListener('click', function() {
+        hideAll();
         toggleText(i);
     });
 }

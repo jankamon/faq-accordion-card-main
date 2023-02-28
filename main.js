a3dbc9eb405dd2f07548ce8f3ffc43637f6dfdfb
janekk <janekkamon@gmail.com>
2023-02-28 11:34:58 +0100

    added button rotation

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const hiddenText = document.getElementsByClassName('hidden-text');
 const textBlock = document.getElementsByClassName('text-block');
 const headers = document.querySelectorAll('h2');
+const btn = document.getElementsByClassName('btn');
 
 const hideAll = () => {
     for(let i = 0; i < textBlock.length; i++){
         hiddenText[i].classList.add('hidden');
         headers[i].style.fontWeight = '400';
+        btn[i].classList.remove('rotated');
     }
 }
 
@@ -13,6 +15,7 @@ const toggleText = (i) => {
     if(hiddenText[i].classList.contains('hidden')){
         hiddenText[i].classList.remove('hidden');
         headers[i].style.fontWeight = '700';
+        btn[i].classList.add('rotated');
     } else {
         hiddenText[i].classList.add('hidden');
         headers[i].style.fontWeight = '400';
